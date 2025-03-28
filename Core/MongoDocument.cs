a774e431ae1d12b63500f51b4b4c23179693ee9d
Kazze <kaze47@gmail.com>
2025-03-28 12:14:43 +0100

    New class MongoDocument

---
+++ Core/MongoDocument.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+
+namespace KazMongoDB.Core
+{
+    public abstract class MongoDocument<T1, T2>  where T2 : MongoDocument<T1, T2>
+    {
+        [BsonIgnore] public static IMongoCollection<T2> Collection { get { return IMongoDocument<T2>.Collection; } }
+        [BsonId] public T1 Id { get; set; }
+
+        public void Insert()
+        {
+            Collection.InsertOne((T2)this);
+        }
+        public void Update()
+        {
+            Collection.ReplaceOne(Builders<T2>.Filter.Eq(x => x.Id, Id), (T2)this);
+        }
+        public void Delete()
+        {
+            Collection.DeleteOne(Builders<T2>.Filter.Eq(x => x.Id, Id));
+        }
+    }
+}
